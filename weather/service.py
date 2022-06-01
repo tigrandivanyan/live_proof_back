a31564101903bdbd267825e0a0674d6bba589be2
Tigran Divanyan <81801870+tigrandivanyan@users.noreply.github.com>
2022-06-01 14:03:42 +0400

    Add files via upload

--- weather/service.py
+++ weather/service.py
@@ -2,24 +2,34 @@ import requests
 import json
 import time
 
-#msk-524901
-#london-2643743
-#NY-5128581
+#Версия от 1.06.22.  
+#Теперь погода запрашивается для любых городов по ID c openweathermap.org
+#Температура теперь округляется правильно и должна совпадать с указанной на сайте. Раньше все, что после запятой просто отбрасывалось.
 
 lastQuery = 0
-cityWeather = {'524901': 0, '2643743': 0, '5128581': 0}
+cityWeather = {}
 
 
 def get_weather(id):
     global lastQuery
     global cityWeather
 
+    newCity = False
+
+    if cityWeather.get(id, 999) == 999:
+        newCity = True
+        print(f'Added city with ID - {id}')
+
     timeDelta = time.time() - lastQuery
+
     if timeDelta > 600:
-        print('Weather refresh')
         lastQuery = time.time()
         for w in cityWeather.keys():
             cityWeather[w] = get_weatherFromWeb(w)
+
+    if newCity == True:
+        cityWeather[id] = get_weatherFromWeb(id)
+        lastQuery = time.time()
 
     return cityWeather[id]
 
@@ -29,8 +39,9 @@ def get_weatherFromWeb(id, api_key='a1612928218374494f0be6aa88b2c9db'):
     url = f'http://api.openweathermap.org/data/2.5/weather?id={id}&units=metric&lang=ru&appid={api_key}'
     response = requests.get(url)
     if response.status_code == 200:
+        print(f'Weather refresh for ID {id} from {url}')
         r_json= json.loads(response.text)
-        temperature = int(r_json['main']['temp'])
+        temperature = round(r_json['main']['temp'])
         if temperature > 0:
             return f'+{temperature}'
         else:
